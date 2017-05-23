39e9d49f07412d66051c43b285401e6f3a6c5beb
Rafka86 <fw.ge-mu.930@jb4.so-net.ne.jp>
2017-05-23 17:44:45 +0900

    Make a sample pidcontrol

--- sample/pidcontrol.c
+++ sample/pidcontrol.c
@@ -5,18 +5,35 @@
 #include "private.h"
 #include "lib/bcontroller.h"
 
+#define SPD_LIM 32767.0
+#define SPD_MIN 400.0
+#define CONTROL_LOOP 1000
+#define GAIN -500.0
+#define KP 1.0
+#define KI 0.5
+#define KD 0.0
+
 void pidcontrol() {
 	DataSet s;
+	double ang_i, spd;
 	int i;
 
-	for (i = 0; i < 10; i++) {
+	ang_i = 0.0;
+	for (i = 0; i < CONTROL_LOOP; i++) {
+		spd = 0.0;
 		if (GetSensorInfos(&s)) return;
-		printf("POS:%lf\n", s.position);
-		printf("VEL:%lf\n", s.velocity);
-		printf("ANG:%lf\n", s.angle);
-		printf("ASP:%lf\n", s.angular_v);
-		sleep(2);
+		
+		ang_i += s.angle;
+		printf("%f\t%f\t%f\n", s.angle, ang_i, s.angular_v);
+		spd = KP * s.angle + KI * ang_i + KD * s.angular_v;
+		spd *= GAIN;
+		spd = (-SPD_MIN < spd && spd < 0) ? -SPD_MIN : (0 < spd && spd < SPD_MIN) ? SPD_MIN : spd;
+		spd = (spd > SPD_LIM) ? SPD_LIM : (spd < -SPD_LIM) ? -SPD_LIM : spd;
+
+		if (Move(spd)) return;
+		usleep(200000);
 	}
+	if (Stop()) return;
 }
 
 int main() {
